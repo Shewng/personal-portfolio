bf5900f2523fbe77ee2ca3c3875f91f54f0df063
Samuel Wong <47337317+Shewng@users.noreply.github.com>
2022-01-03 17:12:17 -0700

    navbar complete (basic)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/components/Navbar.module.scss";
+import { BsList } from "react-icons/bs";
 
 function Navbar() {
+  const [toggler, setToggler] = useState(false);
+
+  const toggleNavbar = () => setToggler((prev) => !prev);
+
   return (
-    <nav>
-      <div className="logo">
-        <Link href="/">
-          <a>
-            <h1>Shwong</h1>
-          </a>
-        </Link>
+    <nav className={styles["navbar"]}>
+      <div className={styles["top-nav"]}>
+        <div className={styles["logo"]}>
+          <Link href="/">
+            <a>
+              <span>Shwong</span>
+            </a>
+          </Link>
+        </div>
+        <div className={styles["nav-dropdown"]} onClick={() => toggleNavbar()}>
+          <BsList size={35} />
+        </div>
       </div>
-      <div className={styles["navbar-links"]}>
-        <Link href="/">
-          <a>home</a>
-        </Link>
-        <Link href="/about">
-          <a>about</a>
-        </Link>
-        <Link href="/projects">
-          <a>projects</a>
-        </Link>
-        <Link href="/contact">
-          <a>contact</a>
-        </Link>
-        <Link href="/resume">
-          <a>resume</a>
-        </Link>
+
+      <div className={styles["links-container"]}>
+        <div
+          className={`${styles["navbar-links"]} ${
+            toggler ? styles["open"] : ""
+          }`}
+        >
+          <Link href="/">
+            <a onClick={() => toggleNavbar()}>home</a>
+          </Link>
+          <Link href="/about">
+            <a onClick={() => toggleNavbar()}>about</a>
+          </Link>
+          <Link href="/projects">
+            <a onClick={() => toggleNavbar()}>projects</a>
+          </Link>
+          <Link href="/contact">
+            <a onClick={() => toggleNavbar()}>contact</a>
+          </Link>
+          <Link href="/resume">
+            <a>resume</a>
+          </Link>
+        </div>
       </div>
     </nav>
   );
