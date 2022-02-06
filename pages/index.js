1277f0190f89ce0da65b892514746c3d91fc2602
Samuel Wong <47337317+Shewng@users.noreply.github.com>
2022-02-06 00:56:26 -0700

    resume update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/pages/Home.module.scss";
 import gif from "../public/nightcity.gif";
+//import vid from "../public/homevid.mp4";
 import { Fade } from "react-reveal";
 import Synopsis from "../components/Synopsis";
 
@@ -9,22 +11,28 @@ export default function Home() {
   return (
     <Fade bottom duration={500} distance={"1em"} delay={150}>
       <Head>
-        <title>Shwong</title>
-        <meta name="description" content="Homepage for The Shwong" />
-        <meta name="keywords" content="shwong" />
+        <title>Samuel Wong</title>
+        <meta
+          name="description"
+          content="A personal showcase of work by Samuel Wong"
+        />
+        <meta name="keywords" content="personal, portfolio, Shwong" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="showDesktop">
         <Synopsis />
       </div>
       <div className={styles["image-container"]}>
-        <Image
+        {/* <Image
           src={gif}
-          alt="Gif of the homepage"
+          alt="Gif of the homepage" 
           layout="fill"
           objectFit="cover"
           quality={100}
-        />
+        /> */}
+        <video preload="" muted autoPlay loop className={styles.video}>
+          <source src="/testerfull1.mp4" />
+        </video>
       </div>
     </Fade>
   );
